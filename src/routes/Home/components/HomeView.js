0c04ebb3d610973688143732726abfc963fe4270
bodyno <az8321550@gmail.com>
2016-08-02 17:38:48 +0800

    Added carousel

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, {Component} from 'react'
+import Carousel from 'nuka-carousel'
 import DuckImage from '../assets/Duck.jpg'
 import classes from './HomeView.scss'
 
-export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className={classes.duck}
-      src={DuckImage} />
-  </div>
-)
-
-export default HomeView
+export default class HomeView extends Component {
+  render () {
+    return (
+      <div>
+        <h4>Welcome!</h4>
+        <img
+          alt='This is a duck, because Redux!'
+          className={classes.duck}
+          src={DuckImage} />
+        <Carousel
+          edgeEasing='easeInSine'
+        >
+          <img src="http://placehold.it/1000x400/cccccc/c0392b/&text=slide1"/>
+          <img src="http://placehold.it/1000x400/aaaaaa/c0392b/&text=slide2"/>
+          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"/>
+          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"/>
+          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5"/>
+          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6"/>
+        </Carousel>
+      </div>
+    )
+  }
+}
