55eaa6307ded9c8901ddd5aed9174c59812c4924
bodyno <az8321550@gmail.com>
2016-08-02 18:14:13 +0800

    Added none webpack plugin

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
 import DuckImage from '../assets/Duck.jpg'
 import classes from './HomeView.scss'
+import flatpickr from 'flatpickr'
+import css from 'flatpickr/dist/flatpickr.min.css'
 
-export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className={classes.duck}
-      src={DuckImage} />
-  </div>
-)
+export default class HomeView extends Component {
 
-export default HomeView
+  componentDidMount() {
+    flatpickr(document.getElementById("time"))
+  }
+
+  render () {
+    return (
+      <div>
+        <h4>Welcome!</h4>
+        <img
+          alt='This is a duck, because Redux!'
+          className={classes.duck}
+          src={DuckImage} />
+        <input id="time" />
+        <h1>123</h1>
+      </div>
+    )
+  }
+}
